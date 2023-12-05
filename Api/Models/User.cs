6c9ee3dbc9807710da538de6f9ebb5f996515050
a.mospan <alexei27039@gmail.com>
2023-12-05 22:27:32 +0200

    чтение из csv

--- Api/Models/User.cs
+++ Api/Models/User.cs
@@ -1,10 +1,13 @@
-﻿namespace Api.Models;
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace Api.Models;
 
 public class User
 {
-    public string Firstname { get; set; }
-
-    public string Surname { get; set; }
-
+    [Index(0)] 
     public string Email { get; set; }
+    [Index(1)]
+    public string Name { get; set; }
+    [Index(2)]
+    public string Group { get; set; }
 }
--- Api/Services/RegisterService.cs
+++ Api/Services/RegisterService.cs
@@ -1,10 +1,28 @@
-﻿namespace Api.Services;
+﻿using System.Globalization;
+using Api.Models;
+using CsvHelper;
+using CsvHelper.Configuration;
+
+namespace Api.Services;
 
 public class RegisterService
 {
-    public async Task<string[]> Registrate(IFormFile file)
+    public async Task<List<User>> Registrate(IFormFile file)
     {
-        var readStream = file.OpenReadStream();
-        readStream.Read()
+        var configuration = new CsvConfiguration(CultureInfo.InvariantCulture)
+        {
+            HasHeaderRecord = false,
+            Delimiter = ";"
+        };
+
+        var records = new List<User>();
+        using (var reader = new StreamReader("filePersons.csv"))
+        using (var csv = new CsvReader(reader, configuration))
+        {
+            records = csv.GetRecords<User>().ToList();
+            return records;
+        }
+
+        
     }
 }
